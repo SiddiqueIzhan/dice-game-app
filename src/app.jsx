812b19fa4c88c9c2b5975de0210a044db3c3d75b
SiddiqueIzhan <izhanvk@gmail.com>
2025-10-02 12:42:06 +0530

    created-dice-app-successfully

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import Intro from "./pages/Intro"
 
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Intro from "./pages/Intro";
+import App from "./app";
 import GamePage from "./pages/GamePage";
 
 const root = document.getElementById("root");
@@ -8,7 +9,7 @@ const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route index element={<Intro />} />
+      <Route index element={<App />} />
       <Route path="game" element={<GamePage />} />
     </Routes>
   </BrowserRouter>
