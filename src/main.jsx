d1298ce8be8bad51d5d872aeceaef69f251a9f0f
SiddiqueIzhan <izhanvk@gmail.com>
2025-10-02 12:24:59 +0530

    created-dice-app-successfully

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import App from "./App";
 import Intro from "./pages/Intro";
 import GamePage from "./pages/GamePage";
 
